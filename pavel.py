ce4b7dbb73cc7682b6acc654c31b9afa32c688b9
Klepikov Dmitriy <thebrootos@gmail.com>
2021-11-28 20:13:42 +0500

    проблема

--- pavel.py
+++ pavel.py
@@ -1,6 +1,7 @@
-def pavel ():
-    return "pavel"
+def get_pavel():
+    """Сделать все по pep8."""
+    return 'pavel'
 
-def dmitriy():
+def get_dmitriy():
     """Сделать все по pep8."""
     return 'dmitriy'  # Переделка со своей ветки!
